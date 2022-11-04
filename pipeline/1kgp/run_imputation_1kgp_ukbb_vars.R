a37de35f3257155e18f9aba56c2873b40d1a5c5b
Andrew Hodges <a.p.hodges@gmail.com>
2022-11-04 16:52:30 -0700

    Migrated older code base.

--- pipeline/1kgp/run_imputation_1kgp_ukbb_vars.R
+++
@@ -1,9 +0,0 @@
-source("/frazer01/projects/CEGS/analysis/hla_type_1kgp/script/functions_hla_type.R")
-
-option_list        = list(make_option("--taskid", type="integer", default=0, help="SGE task ID", metavar="character")) 
-opt_parser         = OptionParser(option_list=option_list)
-opt                = parse_args(opt_parser)
-bin                = opt$taskid
-id2cv              = fread("pipeline/beagle/1kgp_validation/id2cv.txt", sep = "\t", header = TRUE, data.table = FALSE)
-
-impute_1kgp_ukbb_vars(bin, id2cv)
